806a51b44ccbb1c16a1f1dd018c51b1c4d0a5b30
Meet Rakholiya <meet@investifyd.com>
2024-07-08 17:19:09 +0530

    initiat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PasskeyModal  */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image src='/assets/icons/logo-full.svg' alt="paitient" height={1000} width={1000} className="mb-12 h-10 w-fit" />
+          <PatientForm />
+          <div className="text-14-regular mt-2- flex justify-between">
+            <p className="justify-items-center text-dark-600 xl:text-left">© 2024 CarePulse</p>
+            <Link href={'/?admin=true'} className="text-green-500">Admin</Link>
+          </div>
+        </div>
+      </section>
+      <Image src='/assets/images/onboarding-img.png' height={1000} width={1000} alt="patient" className="side-img max-w-[50%]" />
     </div>
   );
 }
---
+++ src/components/CostumFormField.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "./ui/input"
+import { Control } from "react-hook-form"
+import { FormFieldType } from "./forms/PatientForm"
+
+interface CustomProps {
+    control: Control<any>,
+    fieldType: FormFieldType,
+    name: string,
+    label: string,
+    placeholder: string,
+    iconSrc: string,
+    iconAlt: string
+}
+
+const CostumFormField = ({ control, fieldType, name, label, placeholder, iconSrc, iconAlt }: CustomProps) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem className="flex-1">
+                    {fieldType !== FormFieldType.CHECKBOX && label && (
+                        <FormLabel>{label}</FormLabel>
+                    )}
+                </FormItem>
+            )}
+        />
+    )
+}
+
+export default CostumFormField
