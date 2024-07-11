cb0bbcdaa629d3fe25437e39a07ea3c1114f7c42
Meet Rakholiya <meet@investifyd.com>
2024-07-11 11:27:50 +0530

    patient registration complete

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -13,7 +13,7 @@ const SubmitButton = ({ isLoading, className, children }: BUttonProps) => {
       {isLoading ? (
         <div className="flex items-center gap-4">
           <Image
-            src={'assets/icons/loader.svg'}
+            src={'/assets/icons/loader.svg'}
             alt="loader"
             width={24}
             height={24}
